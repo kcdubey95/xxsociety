f151f7bec84c9e9c43f2a0144a6b4475b522030b
kcdubey <dubey.krishnachand@gmail.com>
2019-09-03 23:37:18 +0530

    angular setup

---
+++ application/views/Admin/header.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <script src="../bower_components/angular/angular.js" ></script>
+    <script src="../bower_components/angular-route/angular-route.js" ></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1> head</h1>
+</body>
+</html>
